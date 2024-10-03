32487b1b10e1657f2a2b2ad738434a0b12ac88bc
Mehmet Pürmeşe <mpurmese@gmail.com>
2024-10-04 01:03:15 +0300

    test4

--- src/components/abount/abount.jsx
+++ src/components/abount/abount.jsx
@@ -3,7 +3,6 @@ import { Col, Container, Row } from 'react-bootstrap'
 import AbountImage from './abountImage'
 import AbountTexts from './abountTexts'
 import AbountIcons from './abountIcons'
-import AbountTalent from './abountTalent'
 
 const AbountComponent = () => {
   return (
@@ -39,7 +38,7 @@ const AbountComponent = () => {
         </Row>
         <Row>
             <Col>
-                <AbountTalent/>
+               
             </Col>
         </Row>
     </Container>
